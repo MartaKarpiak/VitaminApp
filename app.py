86968af75acaedc5324a2bcf12f15b8db515f8dd
Marta Karpiak <karpiakmarta20@gmail.com>
2026-05-18 01:06:50 +0300

    Fix Azure SSL connection

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ if os.getenv("GITHUB_ACTIONS") == "true":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = (
-        f'mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}'
-        '?ssl_ca=/etc/ssl/certs/ca-certificates.crt'
+        f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
+        f"?ssl=true"
     )
 
 
